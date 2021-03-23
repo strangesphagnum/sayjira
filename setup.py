0778ddfbc34965ad323702a4338fe548aabb7750
sblokhin@mobiledimension.ru <sblokhin@mobiledimension.ru>
2021-03-23 16:11:53 +0300

    setup and gitignore updated

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "poetry"
+        "pygit2"
     ],
     classifiers=[
         "Development Status :: In Development",
