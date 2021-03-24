23bfb8c820bab410c1490229c9ec5f3e486b36be
sblokhin@mobiledimension.ru <sblokhin@mobiledimension.ru>
2021-03-24 20:20:11 +0300

    Naming issues fix x2

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="say-jira",
+    name="sayjira",
     version="0.0.1",
     description="Adds a jira ticket to the beginning of a commit message",
     author="strangesphagnum",
     keywords="git commit pre-commit hook commit msg message python",
     platforms=["Any"],
     packages=find_packages(include=["sayjira"]),
-    url="https://github.com/strangesphagnum/say-jira",
+    url="https://github.com/strangesphagnum/sayjira",
     install_requires=[
         "pre-commit"
     ],
@@ -20,7 +20,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "sayjira = say-jira.sayjira:main",
+            "sayjira = sayjira.sayjira:main",
         ]
     }
 )
