64c74317669b2b02587ff4bc353fd2902c2e2deb
sblokhin <sblokhin@mobiledimension.ru>
2021-03-26 13:59:22 +0300

    Prepare message -> post commit

--- sayjira/sayjira.py
+++ sayjira/sayjira.py
@@ -6,21 +6,28 @@ from pathlib import Path
 import git
 
 
+repo = git.Repo(".")
+
+
 def get_jira_ticket(branch_name):
     jira_ticket = re.match(r"((?<!([A-Z]\{1,10\})-\?)[A-Z]+-\d+)", branch_name)
     if jira_ticket:
         return jira_ticket.group(0)
 
     
+# def update_commit_message(branch, jira_ticket):
+#     message_path = sys.argv[1]
+#     with open(message_path, "w") as message_file:
+#         message = message_file.read()
+#         message_file.write(f"[{jira_ticket}] {message}")
+
 def update_commit_message(branch, jira_ticket):
-    message_path = sys.argv[1]
-    with open(message_path, "w") as message_file:
-        message = message_file.read()
-        message_file.write(f"[{jira_ticket}] {message}")
+    commit = repo.head.commit
+    message = commit.message
+    commit.message = f"[{jira_ticket}] {message}"
 
 
 def main():
-    repo = git.Repo(".")
     branch = repo.head.reference
     jira_ticket = get_jira_ticket(branch.name)
     if jira_ticket:
