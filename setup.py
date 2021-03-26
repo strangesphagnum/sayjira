0182ec08f0eec5d1d82ee6989d53282991e8a845
sblokhin <sblokhin@mobiledimension.ru>
2021-03-26 15:40:15 +0300

    Freakin version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sayjira",
-    version="0.0.15",
+    version="0.0.16",
     description="Adds a jira ticket from branch name to the beginning of a commit message",
     author="strangesphagnum",
     keywords="git commit pre-commit hook commit msg message python",
