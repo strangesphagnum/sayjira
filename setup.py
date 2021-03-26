ca075d3db1067207fbecdfe60ad0c13d0703f52e
sblokhin <sblokhin@mobiledimension.ru>
2021-03-26 15:32:06 +0300

    Version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sayjira",
-    version="0.0.13",
+    version="0.0.14",
     description="Adds a jira ticket from branch name to the beginning of a commit message",
     author="strangesphagnum",
     keywords="git commit pre-commit hook commit msg message python",
